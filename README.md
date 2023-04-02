87d4a8611781d3b510228e188ef63d1339a52a12
Jannis Jung <jannis@Air-von-Jannis.fritz.box>
2023-04-02 16:01:26 +0200

    First Window | Build Configuration

---
+++ README.md
@@ -0,0 +1,4 @@
+# Vulkan Raytracing with compute shaders
+
+## In the future
+- Add Vulkan hardware accelerated Raytracing capabilities
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,25 @@
-int main(int argc, char** argv){
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
     return 0;
 }
